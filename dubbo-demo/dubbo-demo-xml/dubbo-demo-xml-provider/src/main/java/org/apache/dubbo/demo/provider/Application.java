09e3f9f65920ae3be661dfec784dd3510d321dd7
desertsnow917 <466131874@qq.com>
2019-10-01 18:16:45 +0800

    Hello Dubbo xml provider.

--- dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
+++ dubbo-demo/dubbo-demo-xml/dubbo-demo-xml-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
@@ -20,6 +20,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args) throws Exception {
+        ///
+        // Hello Dubbo xml provider
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/dubbo-provider.xml");
         context.start();
         System.in.read();
